20e6132c6ca92494f90c09e1be3a063b247b8c84
Mauricio Garcia <45175048+MauricioGR15@users.noreply.github.com>
2020-03-21 19:39:26 -0700

    Inserciones en base de datos para trabajar con consultas listo

---
+++ ConsultasComedor.sql
@@ -0,0 +1,302 @@
+use comedorpinkflor
+/* 1. obtener los nombres de los tutores que deben dinero durante el mes de marzo del 2020*/
+SELECT t.tutor_nombre FROM tutores t
+inner JOIN Pagos p
+on t.tutor_RFC = p.tutor_rfc
+WHERE MONTH(p.pago_fecha) = 3
+/* 2. obtener el nombre de los alumnos cuyos tutores deben mas de $100*/
+SELECT a.alu_nombre FROM Alumnos a
+WHERE a.tutor_RFC in 
+(
+    select Pagos.tutor_rfc FROM Pagos
+    WHERE Pagos.pago_cantidad > 100
+)
+/* 3.Obtener el ingrediente mas utilizado en los menus que tienen adeudos los tutores */
+GO
+WITH ingredientesMenusPagos(ingredientesP)
+as(
+SELECT i.ing_nombre FROM Pagos p
+JOIN MenuContenido mc on p.menu_id = mc.menu_id
+JOIN AlimentoContenido ac on mc.ali_ID = ac.ali_id
+join Ingredientes i on i.ing_id = ac.ing_id
+)
+SELECT top 1 ingredientesP from ingredientesMenusPagos
+GROUP BY ingredientesP 
+ORDER by COUNT(ingredientesP) DESC
+/* 4.Obtener el nombre del tutor que debe mas dinero */
+GO
+with adeudostotal(tutor_rfc,total)
+as
+(
+    select pagos.tutor_rfc,sum(Pagos.pago_cantidad) FROM Pagos
+    GROUP BY Pagos.tutor_rfc
+)
+SELECT Tutores.tutor_nombre FROM Tutores
+WHERE Tutores.tutor_RFC = 
+(
+    select top 1 adeudostotal.tutor_rfc FROM adeudostotal
+    ORDER BY adeudostotal.total desc
+)
+/*5. Cuantas personas deben por cada concepto  */
+SELECT Pagos.pago_concepto,COUNT(Pagos.pago_concepto) as Tutores from Pagos
+GROUP BY Pagos.pago_concepto
+
+/*6. Obtener los alimentos que tienen "pollo" como ingrediente y tienen mas de 300 calorias */
+SELECT a.ali_nombre FROM Alimentos a
+INNER JOIN AlimentoContenido ac on ac.ali_id = a.ali_ID
+INNER JOIN Ingredientes i on i.ing_id = ac.ing_id
+WHERE i.ing_nombre = 'pollo' and a.ali_calorias > 300
+
+/*7. Obtener el ingrediente mas usado en los alimentos */
+go
+with cantidadIngredientes(nombre,cantidad)
+AS
+(
+    SELECT Ingredientes.ing_nombre,COUNT(AlimentoContenido.ing_id) FROM Ingredientes
+    JOIN AlimentoContenido ON Ingredientes.ing_id = AlimentoContenido.ing_id
+    JOIN Alimentos on Alimentos.ali_ID = AlimentoContenido.ali_id
+    GROUP BY Ingredientes.ing_nombre
+)
+SELECT top 1 * from cantidadIngredientes
+ORDER BY cantidadIngredientes.cantidad DESC
+
+/*8. Saber que alimentos estan almenos en 2 menus */
+go
+WITH mismosalimentos(alimentos,menus)
+as(
+SELECT MenuContenido.ali_ID,COUNT(MenuContenido.ali_ID) as menus from MenuContenido
+GROUP BY MenuContenido.ali_ID
+)
+SELECT*FROM mismosalimentos
+WHERE menus > 2
+
+select * from Alimentos
+select * from MenuContenido
+select *from menus
+
+/*9. obtener las bebidas que estan en el menu mas caro */
+SELECT Alimentos.ali_nombre as bebidas FROM Alimentos
+WHERE ali_tipo = 'B' and ali_ID IN
+(
+SELECT top 1 MenuContenido.menu_id from MenuContenido
+JOIN Menus on MenuContenido.menu_id = Menus.menu_id
+ORDER by Menus.menu_costo DESC
+)
+
+select Alimentos.ali_nombre as Bebida from Alimentos
+where ali_tipo = 'B' and ali_id in (
+select ali_id as bebidas from MenuContenido
+join Menus on MenuContenido.menu_id = Menus.menu_id
+where (Menus.menu_id = (select top 1 Menu_id from Menus order by Menu_costo desc)))
+
+/*10. obtener el nombre de los alumnos que tienen 'pastel' como postre */
+use ComedorPinkFlor
+SELECT Alumnos.alu_nombre FROM Alumnos
+WHERE Alumnos.alu_matricula IN
+(
+select p.alu_matricula FROM pagos p
+JOIN MenuContenido mc on mc.menu_id = p.menu_id
+JOIN Alimentos a on a.ali_ID = mc.ali_ID
+WHERE a.ali_nombre = 'pastel' and a.ali_tipo = 'p'
+
+)
+/*11.Obtener el nombre de los tutores el dia donde mas ventas hubo */
+GO
+WITH masventas(pagos,fecha)
+as(
+select COUNT(Pagos.pago_ID),Pagos.pago_fecha FROM Pagos
+GROUP BY Pagos.pago_fecha
+)
+SELECT t.tutor_nombre,mv.pagos FROM Pagos p 
+JOIN Tutores t on t.tutor_RFC = p.tutor_rfc
+JOIN masventas mv on mv.fecha = p.pago_fecha
+WHERE p.pago_fecha = 
+(
+SELECT top 1 masventas.fecha FROM masventas
+ORDER BY masventas.pagos DESC
+)
+/*12.Obtener el nombre de los alumnos que comieron del mismo menu*/
+SELECT Alumnos.alu_nombre, Pagos.menu_id FROM Alumnos
+inner JOIN Pagos on Pagos.alu_matricula = Alumnos.alu_matricula
+GROUP BY Pagos.menu_id, Alumnos.alu_nombre
+
+/*13.Obtener el ID de los menus que tengan alimentos con
+mas de 500 calorias durante el mes de enero*/
+SELECT m.menu_id FROM Menus m
+INNER JOIN MenuContenido mc on mc.menu_id = m.menu_id
+INNER JOIN Alimentos a on a.ali_ID =mc.ali_ID
+WHERE ali_calorias > 500
+
+/*14 obtener las ganancias promedios durante el mes de marzo de cada grado*/
+SELECT a.alu_grado,avg(p.pago_cantidad) as alumnos FROM Pagos p
+INNER JOIN Alumnos a on a.alu_matricula = p.alu_matricula
+WHERE year (p.pago_fecha) =1
+GROUP by a.alu_grado
+
+/*15 obtener el alumno de cada grado que haya tenido el menu mas caro*/
+GO
+WITH AlumnoMasCaro (costo)
+AS(
+    SELECT MAX(m.menu_costo)from Menus m
+    JOIN Pagos p on m.menu_id = p.menu_id
+    JOIN Alumnos a on p.alu_matricula = a.alu_matricula
+    GROUP BY a.alu_grado
+)
+SELECT Alumnos.alu_nombre,Alumnos.alu_grado FROM Alumnos
+INNER JOIN Pagos on Alumnos.alu_matricula = Pagos.alu_matricula
+WHERE Pagos.pago_cantidad IN
+(
+    select*FROM AlumnoMasCaro
+)
+GROUP BY Alumnos.alu_grado,Alumnos.alu_nombre
+/*16 Obetner el total de ventas de cada uno de los menus durante el mes de marzo*/
+SELECT Menus.menu_renglon,count(Pagos.menu_id) as ventas FROM Menus 
+inner JOIN Pagos on Pagos.menu_id = Pagos.menu_id
+WHERE MONTH(Pagos.pago_fecha) =3
+GROUP by  Menus.menu_renglon 
+
+/*17 otener el grado que tenga el mayor numero de 
+ alumnos que han consumido de un menu especial*/
+SELECT top 1 Alumnos.alu_grado,Pagos.pago_cantidad FROM Alumnos
+JOIN Pagos on Pagos.alu_matricula = Alumnos.alu_matricula
+join Menus on Menus.menu_id = pagos.menu_id
+WHERE Menus.menu_tipo = 0
+ORDER BY Pagos.pago_cantidad DESC
+
+/*18 obtener el nombre de los alumnos cuyos tutores trabajen donde mismo*/
+GO
+WITH MismoTrabajo(trabajo,tutores)
+as
+(
+select tutor_trabajo,COUNT(tutor_trabajo) as tutores from Tutores
+    GROUP BY tutor_trabajo
+)
+SELECT Alumnos.alu_nombre FROM Alumnos
+INNER JOIN Tutores on Alumnos.tutor_RFC = Tutores.tutor_RFC
+WHERE Tutores.tutor_trabajo in
+(
+    select MismoTrabajo.trabajo from MismoTrabajo
+    WHERE MismoTrabajo.tutores > 1
+)
+/*19 obtener los numeros de telefono de los tutores de los alumnos
+ que sufren alguna alergia*/
+
+ SELECT tt.tutor_telefono,t.tutor_nombre from TelefonosTutores tt
+ INNER JOIN Tutores t on t.tutor_RFC = tt.tutor_RFC
+ INNER JOIN Alumnos a on t.tutor_RFC = a.tutor_RFC
+ INNER JOIN Alergias al on al.alu_matricula = a.alu_matricula
+ GROUP BY t.tutor_nombre,tt.tutor_telefono
+
+/*20. obtener los postres que estuvieron en un menu especial */
+SELECT a.ali_nombre,case a.ali_tipo when 'P' then 'Postre' end as tipo
+FROM Alimentos a
+INNER JOIN MenuContenido mc on mc.ali_ID = a.ali_ID
+INNER JOIN Menus m on m.menu_id = mc.menu_id
+WHERE m.menu_tipo = 0 and a.ali_tipo = 'P'
+
+/*21. obtener el alimento con menos grasas de cada uno de los menus */
+select alimentos.ali_nombre,alimentos.ali_grasas,case alimentos.ali_tipo 
+when 'P' then 'Postre'
+when 'B' then 'Bebida'
+when 'C' then 'Comida'
+end as tipos,
+Pagos.menu_id FROM Pagos
+JOIN MenuContenido on MenuContenido.menu_id = Pagos.menu_id
+JOIN Alimentos on Alimentos.ali_ID = MenuContenido.ali_ID
+WHERE Alimentos.ali_grasas IN
+(
+    select MIN(Alimentos.ali_grasas) from Pagos
+    JOIN MenuContenido mc on mc.menu_id = Pagos.menu_id
+    JOIN Alimentos on Alimentos.ali_ID = mc.ali_ID
+    GROUP BY Pagos.menu_id
+)
+
+/*22. Obtner a los tutores que han gastado mas de $3000 en los ultimos 3 meses y
+ darles un descuento del 10% y mostrar el total con el descuento*/
+SELECT t.tutor_nombre,t.tutor_apellidoP,t.tutor_apellidoM,pago_cantidad,
+(pago_cantidad*.9) as Con_Desc FROM Tutores t
+join Pagos p on p.tutor_rfc = t.tutor_RFC
+WHERE p.pago_cantidad >= 3000 and
+(MONTH(p.pago_fecha) < GETDATE() and MONTH(p.pago_fecha)>DATEADD(m,-3,GETDATE()))
+
+/*23. Obtener los nombres completos de los alumnos del grupo donde hay mas 
+alumnos con alergias*/
+GO
+with GrupoMasAlgergias(grupo,NumAlumnos)
+as
+(
+  select top 1 Alumnos.alu_grupo,COUNT(Alumnos.alu_grupo) as alumnos FROM  Alumnos
+    JOIN Alergias on Alumnos.alu_matricula = Alergias.alu_matricula
+    GROUP by Alumnos.alu_grupo
+    ORDER BY alumnos desc
+)
+select CONCAT(a.alu_apellidoP,' ',a.alu_apellidoM,' ',alu_nombre) as Nombre_completo from Alumnos a
+where a.alu_grupo = 
+(
+  select GrupoMasAlgergias.grupo FROM GrupoMasAlgergias
+)
+/*24. Conocer que tipo de menu fue el mas vendido en el ultimo mes y cuanto genero*/
+GO
+with MenuMasVendido (MenuTipo,cantidad)
+as 
+(
+    SELECT top 1 m.menu_tipo,(m.menu_tipo) as cantidad FROM Menus m
+    JOIN Pagos p on p.menu_id = m.menu_id
+    GROUP BY m.menu_tipo
+    ORDER BY cantidad desc
+)
+SELECT case m.menu_tipo 
+when 0 then 'Especial'
+when 1 then 'Notmal'end as Tipo,
+SUM(p.pago_cantidad) as Total
+from Menus m
+JOIN Pagos p on p.menu_id = m.menu_id
+WHERE m.menu_tipo =
+(
+select MenuMasVendido.MenuTipo FROM MenuMasVendido
+)
+GROUP by m.menu_tipo
+
+/*25. Obtener el nombre de los ingredientes que no han estado en ningun menu*/
+SELECT i.ing_nombre FROM Ingredientes i
+WHERE i.ing_id not IN
+(
+ select ac.ing_id from AlimentoContenido ac
+ JOIN MenuContenido mc on mc.ali_ID = ac.ali_id
+)
+
+/*26. ver en cuantas veces aparece cada alimento en distintos menus*/
+SELECT a.ali_nombre,COUNT(a.ali_nombre) as Veces_Repetidas FROM Alimentos a
+JOIN MenuContenido mc on a.ali_ID =mc.ali_ID
+GROUP BY a.ali_nombre
+
+/*27. Obtener el nombre completo de los tutores que nunca han pedido un menu especial*/
+SELECT t.tutor_nombre+' '+t.tutor_apellidoP+' '+coalesce(t.tutor_apellidoM,' ') as NombreCompleto
+FROM Tutores t JOIN Pagos p on p.tutor_rfc = t.tutor_RFC
+JOIN Menus m on m.menu_id = p.menu_id
+WHERE m.menu_tipo != 0
+
+/*28.Obtener las fechas en los que se sirvieron hamburguesas a los alumno de 5to
+y 6t0 grado*/
+SELECT p.pago_fecha,a.alu_nombre,a.alu_grado from pagos p
+JOIN Alumnos a on a.alu_matricula = p.alu_matricula
+JOIN MenuContenido mc on mc.menu_id = p.menu_id
+JOIN Alimentos al on al.ali_ID = mc.ali_ID
+WHERE (a.alu_grado = 5 OR a.alu_grado = 6)
+AND al.ali_nombre = 'hamburguesa'
+
+/*28.Obtener las ganancias mensuales de los menus normales y menus especiales*/
+go
+WITH GananciasMensualesMenu(Tipo,Mes,Cantidad)
+as(
+SELECT case m.menu_tipo when 0 then 'Especial' when 1 then 'Normal' end as Tipo ,
+month(p.pago_fecha) as Mes ,p.pago_cantidad FROM Menus m
+JOIN Pagos p on p.menu_id = m.menu_id
+group BY MONTH(p.pago_fecha),m.menu_tipo,p.pago_cantidad
+)
+SELECT*FROM GananciasMensualesMenu gmm
+PIVOT(sum(gmm.Cantidad) FOR Tipo IN (Especial,Normal)) 
+as GananciasPivot
+
+
+
--- DATOS.sql
+++ DATOS.sql
@@ -7,6 +7,7 @@ insert into Tutores values
 	('PILR750814JD3','Rodrigo','Piedras','Lara','Gerente'),
 	('AGAT790115LR3','Tomasa','Aguilar','Arreaga','Vendedora')
 
+
 insert into TelefonosTutores values 
 	('MALO871112LF5',6672154623),
 	('MALO871112LF5',6678154231),
@@ -24,6 +25,16 @@ insert into Alumnos values
 	(171567,'Gabriela','Lopez','Escobar',2,'B','LOMT920505LO5'),
 	(181517,'Evelyn','Piedras','Sainz',3,'D','PILR750814JD3'),
 	(201618,'Jason','Aguilar','Martinez',6,'A','AGAT790115LR3')
+
+insert into Menus values 
+	(1,1,450),
+	(2,1,460),
+	(3,1,454),
+	(4,0,670),
+	(5,1,600),
+	(6,1,570),
+	(7,0,650)
+
 
 insert into Ingredientes values 
 	('Pimienta',14,'20220220',0),
@@ -43,9 +54,22 @@ insert into Ingredientes values
 	('Pastor',23,'20200330',1),
 	('Leche',50,'20200815',0),
 	('Azucar',30,'20210916',1)
+	
 
-	select * from IngredienteMedida
-
+	insert into Ingredientes values
+	('Zanahoria', 25, '2020-05-25', 0),
+	('Frijol', 28, '2020-04-10', 0),
+	('Salchicha de puerco', 26, '2020-06-06', 1),
+	('Tocino', 27, '2020-04-05', 1),
+	('Cilantro', 22, '2020-05-12', 1),
+	('Aguacate', 22, '2020-05-13', 0),
+	('Tortilla', 30, '2020-06-10', 0),
+	('Cebada', 31, '2020-07-15', 0),
+	('Arroz', 28, '2020-05-01', 0),
+	('Harina', 23, '2020-12-28', 0),
+	('Huevos', 25, '2020-05-03', 0),
+	('Extracto de vainilla', 27, '2020-04-02', 1),
+	('Chocolate amargo', 21, '2020-05-05', 1)
 
 insert into IngredienteMedida values
 	(1,'Gr'),
@@ -70,4 +94,97 @@ insert into Alergias values
 	(171567,13,'Salen ronchas en la piel'),
 	(171567,15,'Ojos hinchados'),
 	(181517,1,'Vomito y tos')
+
+
+--ALIMENTOS
+insert into Alimentos values
+	('Sopa de verduras','C',35,442,25,10),
+	('Carne en su jugo','C',42,430,29,16),
+	('Tamal de puerco','C',28,445,7,30),
+	('Sopa de tortillas','C',28,423,8,4),
+	('Asado','C',34,440,34,26),
+	('Sirloin','C',35,435,36,25),
+	('Papa con carne','C',27,380,32,18),
+	('Tostadas de frijol','C',20,280,10,15),
+	('Tacos dorados','C',34,350,10,25),
+	('Ceviche','C',18,215,30,0),
+	('Hamburguesa','C',55,540,15,60),
+
+	('Refresco','B',10,41,1,1),
+	('Cebada','B',6,30,3,0),
+	('Horchata','B',7,30,3,2),
+	('Jamaica','B',7,40,4,0),
+	('Te Jazmin','B',9,32,4,0),
+	('Agua embotellada','B',0,0,0,0),
+	('Limonada','B',5,30,1,0),
+
+	('Suffle','P',30,278,5,1),
+	('Pastel de vainilla','P',42,300,6,2),
+	('Pastel de chocolate','P',50,345,5,3),
+	('Brownie','P',38,285,4,2),
+	('Crepa','P',40,289,3,1),
+	('Waffle','P',37,259,5,3),
+	('Nieve','P',28,180,5,1)
+
+--PAGOS
+INSERT INTO Pagos values (1, 'MALO871112LF5', 191618, 800, 1000, 'Mensual', '20200315')
+INSERT INTO Pagos values (2, 'MALO871112LF5', 191618, 600, 800, 'Mensual', '20200415')
+INSERT INTO Pagos values (2, 'PESA901212FE5', 191413, 1800, 2000, 'Trimestral', '20200229')
+INSERT INTO Pagos values (3, 'LOMT920505LO5', 171567, 2500, 2500, 'Semestral', '20200430')
+INSERT INTO Pagos values (1, 'PILR750814JD3', 181517, 1600, 1400, 'Bimestral', '20200303')
+INSERT INTO Pagos values (4, 'PILR750814JD3', 181517, 750, 800, 'Mensual', '20200403')
+INSERT INTO Pagos values (6, 'AGAT790115LR3', 201618, 700, 700, 'Mensual', '20200228')
+INSERT INTO Pagos values (4, 'MALO871112LF5', 191720, 1500, 1200, 'Bimestral', '20200320')
+INSERT INTO Pagos values (2, 'AGAT790115LR3', 201618, 1800, 1500, 'Trimestral', '20200328')
+INSERT INTO Pagos values (7, 'PESA901212FE5', 181417, 850, 800, 'Mensual', '20200302')
+
+
+--Sopa de verduras
+INSERT INTO AlimentoContenido values (1, 5, 100)
+INSERT INTO AlimentoContenido values (1, 6, 50)
+INSERT INTO AlimentoContenido values (1, 3, 10)
+INSERT INTO AlimentoContenido values (1, 18, 50)
+INSERT INTO AlimentoContenido values (1, 10, 50)
+INSERT INTO AlimentoContenido values (1, 11, 50)
+INSERT INTO AlimentoContenido values (1, 12, 50)
+INSERT INTO AlimentoContenido values (1, 1, 2)
+INSERT INTO AlimentoContenido values (1, 4, 5)
+
+--Carne en su jugo
+INSERT INTO AlimentoContenido values (2, 20, 100)
+INSERT INTO AlimentoContenido values (2, 21, 100)
+INSERT INTO AlimentoContenido values (2, 19, 200)
+INSERT INTO AlimentoContenido values (2, 6, 50)
+INSERT INTO AlimentoContenido values (2, 1, 2)
+INSERT INTO AlimentoContenido values (2, 4, 5)
+
+--Sopa de tortillas
+INSERT INTO AlimentoContenido values (3, 5, 100)
+INSERT INTO AlimentoContenido values (3, 3, 10)
+INSERT INTO AlimentoContenido values (3, 6, 100)
+INSERT INTO AlimentoContenido values (3, 22, 25)
+INSERT INTO AlimentoContenido values (3, 23, 100)
+INSERT INTO AlimentoContenido values (3, 24, 200)
+
+--Cebada
+INSERT INTO AlimentoContenido values (13, 25, 150)
+INSERT INTO AlimentoContenido values (13, 17, 25)
+
+--Horchata
+INSERT INTO AlimentoContenido values (15, 26, 100)
+INSERT INTO AlimentoContenido values (15, 17, 10)
+
+--Pastel de vainilla
+INSERT INTO AlimentoContenido values (21, 27, 200)
+INSERT INTO AlimentoContenido values (21, 17, 50)
+INSERT INTO AlimentoContenido values (21, 28, 100)
+INSERT INTO AlimentoContenido values (21, 29, 10)
+INSERT INTO AlimentoContenido values (21, 16, 100)
+
+--Pastel de chocolate
+INSERT INTO AlimentoContenido values (21, 27, 200)
+INSERT INTO AlimentoContenido values (21, 17, 50)
+INSERT INTO AlimentoContenido values (21, 28, 100)
+INSERT INTO AlimentoContenido values (21, 30, 100)
+INSERT INTO AlimentoContenido values (21, 16, 100)
 
